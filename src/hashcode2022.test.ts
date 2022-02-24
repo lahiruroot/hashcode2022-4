d63334b533ac2c5717fd5aac645184fc195dcb5b
Erik Hansson <hansson_erik@outlook.com>
2022-02-24 22:02:24 +0100

    new

--- src/hashcode2022.test.ts
+++ src/hashcode2022.test.ts
@@ -1,18 +1,18 @@
 import { runSimulation } from "runSimulation";
 import { readFile, writeFile, printHeader } from "./utils";
 
-test("a_an_example.in.txt", async () => {
+test.only("a_an_example.in.txt", async () => {
   const testName = expect.getState().currentTestName;
   const input = await readFile(testName);
   printHeader("Input", testName);
-  //process.stdout.write(input + "\n");
+  process.stdout.write(input + "\n");
 
   printHeader("Debug", testName);
   const result = await runSimulation(input, { apa: 0 });
 
   const resultName = `${testName}.Result`;
   printHeader("Result", testName);
-  //process.stdout.write(result + "\n");
+  process.stdout.write(result + "\n");
 
   printHeader("End", testName);
   await writeFile(result, resultName);
--- src/runSimulation.ts
+++ src/runSimulation.ts
@@ -4,6 +4,7 @@ interface Skill {
 }
 interface Contributor {
   name: string;
+  daysToAvailable: number;
   skills: Skill[];
 }
 interface Role {
@@ -14,7 +15,9 @@ interface Project {
   name: string;
   durationDays: number;
   score: number;
+  assignees: Map<string, Contributor>;
   bestBefore: number;
+  workedDays: number;
   roles: Skill[];
 }
 
@@ -69,7 +72,7 @@ const parse = (input: string): Model => {
       currentContributor === undefined
     ) {
       const [name, numberOfSkills] = currentRow.split(" ");
-      currentContributor = { name, skills: [] };
+      currentContributor = { name, skills: [], daysToAvailable: 0 };
       numberOfSkillsToParse = Number(numberOfSkills);
     } else if (currentContributor && numberOfSkillsToParse > 0) {
       numberOfSkillsToParse--;
@@ -91,6 +94,8 @@ const parse = (input: string): Model => {
         bestBefore: Number(bestBefore),
         score: Number(maxPoints),
         durationDays: Number(duration),
+        assignees: new Map(),
+        workedDays: 0,
       };
       numberOfSkillsToParse = Number(numberOfSkills);
     } else if (currentProject && numberOfSkillsToParse > 0) {
@@ -111,28 +116,110 @@ const parse = (input: string): Model => {
 const getWeight = (project: Project) => {
   return project.score / project.durationDays;
 };
+//const run_old = (model: Model, {}: RunSimulationParams) => {
+//  const sortedProjects = [...model.projects].sort((a, b) =>
+//    getWeight(a) > getWeight(b) ? 1 : -1
+//  );
+//  const result = sortedProjects.map((proj) => {
+//    const plannedProject: PlannedProject = {
+//      name: proj.name,
+//      roles: proj.roles.map((role) => {
+//          model.contributors.
+//      }),
+//      roles: model.contributors.filter((c) =>
+//        c.skills.some(
+//          (skill) =>
+//            proj.roles.find(
+//              (role) => role.name === skill.name && role.level <= skill.level
+//            ) !== undefined
+//        )
+//      ),
+//    };
+//    return plannedProject;
+//  });
+//
+//  //console.warn({ result: JSON.stringify(result, null, 2) });
+//  return result;
+//};
+//
+//
 const run = (model: Model, {}: RunSimulationParams) => {
-  const sortedProjects = [...model.projects].sort((a, b) =>
-    getWeight(a) > getWeight(b) ? 1 : -1
-  );
-  const result = sortedProjects.map((proj) => {
-    const plannedProject: PlannedProject = {
-      name: proj.name,
-      roles: model.contributors.filter((c) =>
-        c.skills.some(
-          (skill) =>
-            proj.roles.find(
-              (role) => role.name === skill.name && role.level <= skill.level
-            ) !== undefined
-        )
-      ),
-    };
-    return plannedProject;
-  });
+  let projects: Array<Project> = model.projects;
+  let contributors: Array<Contributor> = model.contributors;
 
-  //console.warn({ result: JSON.stringify(result, null, 2) });
+  let day = 0;
+
+  const result: PlannedProject[] = [];
+  while (projectsLeft(projects).length > 0 && day < 50) {
+    day++;
+    projectsLeft(projects)
+      .filter((x) => x.workedDays > 0)
+      .forEach((project) => {
+        ++project.workedDays;
+
+        project.assignees.forEach((value, key) => {
+          --value.daysToAvailable;
+          if (project.workedDays === project.durationDays) {
+            let skillLevel = value.skills.find((s) => s.name === key);
+            if (
+              (skillLevel?.level ??
+              0) <= (project.roles.find((s) => s.name === key) ?? 0)
+            ) {
+              if (!skillLevel) {
+                value.skills.push({ name: key, level: 1 });
+              } else {
+                skillLevel.level++;
+              }
+            }
+          }
+        });
+      });
+
+    const availableProjects = notStartedProjects(projects);
+
+    availableProjects.forEach((selectedProject) => {
+      const projectSkills = selectedProject.roles;
+
+      const availableProjectContributors = contributors.filter(
+        (x) => x.daysToAvailable === 0
+      );
+      for (let skill of projectSkills) {
+        const availableContributors = availableProjectContributors.filter(
+          (x) => !Array.from(selectedProject.assignees.values()).includes(x)
+        );
+        const mentors = availableContributors.filter((y) =>
+          canMentor(skill, y)
+        );
+        if (mentors.length > 0) {
+          selectedProject.assignees.set(skill.name, mentors[0]);
+        } else {
+          selectedProject.assignees = new Map();
+          return;
+        }
+      }
+      if (selectedProject.assignees.size === selectedProject.roles.length) {
+        selectedProject.assignees.forEach(
+          (x) => (x.daysToAvailable = selectedProject.durationDays)
+        );
+        selectedProject.workedDays = 1;
+        result.push({
+          roles: Array.from(selectedProject.assignees.values()),
+          name: selectedProject.name,
+        });
+      }
+    });
+    console.warn({day, result})
+  }
   return result;
 };
+
+const projectsLeft = (projects: Array<Project>) =>
+  projects.filter((x) => x.durationDays > x.workedDays);
+const notStartedProjects = (projects: Array<Project>) =>
+  projects.filter((x) => x.workedDays === 0);
+const canMentor = (projectSkill: Skill, contributor: Contributor) =>
+  projectSkill.level <=
+  (contributor.skills.find((x) => x.name === projectSkill.name) ?? 0);
 
 interface RunSimulationParams {}
 
