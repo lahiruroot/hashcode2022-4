6ed4376db4735576ed61d77090edbd640679bbb6
Erik Hansson <hansson_erik@outlook.com>
2022-02-24 18:49:53 +0100

    input

--- src/hashcode2022.test.ts
+++ src/hashcode2022.test.ts
@@ -1,7 +1,24 @@
 import { runSimulation } from "runSimulation";
 import { readFile, writeFile, printHeader } from "./utils";
 
-test.only("exampleInput", async () => {
+test("exampleInput", async () => {
+  const testName = expect.getState().currentTestName;
+  const input = await readFile(testName);
+  printHeader("Input", testName);
+  process.stdout.write(input + "\n");
+
+  printHeader("Debug", testName);
+  const result = await runSimulation(input, { apa: 0 });
+
+  const resultName = `${testName}.Result`;
+  printHeader("Result", testName);
+  process.stdout.write(result + "\n");
+
+  printHeader("End", testName);
+  await writeFile(result, resultName);
+});
+
+test.only("a_an_example.in.txt", async () => {
   const testName = expect.getState().currentTestName;
   const input = await readFile(testName);
   printHeader("Input", testName);
